4f3a7451a97fa3547115984801d37c1754fbc254
Maxim Shatunov <mshatunov@alfabank.ru>
2017-08-13 00:09:47 +0300

    Lesson4. Added reflection call of method

--- src/main/java/ru/mshatunov/basejava/MainReflection.java
+++ src/main/java/ru/mshatunov/basejava/MainReflection.java
@@ -3,6 +3,8 @@ package ru.mshatunov.basejava;
 import ru.mshatunov.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
@@ -14,7 +16,18 @@ public class MainReflection {
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "uuid1");
-        //TODO invoke r.toString via reflection
+        Method[] methods = r.getClass().getMethods();
+        for (Method m : methods) {
+            if (m.getName().equals("toString")) {
+                try {
+                    System.out.print("Reflection toString: ");
+                    System.out.println(m.invoke(r));
+                    break;
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         System.out.println(field.get(r));
     }
 
