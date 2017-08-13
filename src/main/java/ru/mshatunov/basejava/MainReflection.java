2cad45410568f4b43e0c5c97fb90c823f2bc989c
Maxim Shatunov <mshatunov@alfabank.ru>
2017-08-13 20:36:16 +0300

    Lesson4. Reflection refactoring

--- src/main/java/ru/mshatunov/basejava/MainReflection.java
+++ src/main/java/ru/mshatunov/basejava/MainReflection.java
@@ -8,27 +8,22 @@ import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
 
         Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Class resumeClass = r.getClass();
+
+        Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "uuid1");
-        Method[] methods = r.getClass().getMethods();
-        for (Method m : methods) {
-            if (m.getName().equals("toString")) {
-                try {
-                    System.out.print("Reflection toString: ");
-                    System.out.println(m.invoke(r));
-                    break;
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
         System.out.println(field.get(r));
+
+        Method method = resumeClass.getMethod("toString");
+        System.out.print("Reflection toString: ");
+        System.out.println(method.invoke(r));
+
     }
 
 }
