17beae0d9947ffaf577dc2fde39f6fdf19e01076
Maxim Shatunov <mshatunov@alfabank.ru>
2017-11-01 13:53:57 +0300

    Lesson 8. Update model

--- src/main/java/ru/mshatunov/basejava/model/Resume.java
+++ src/main/java/ru/mshatunov/basejava/model/Resume.java
@@ -1,5 +1,7 @@
 package ru.mshatunov.basejava.model;
 
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class Resume implements Comparable<Resume> {
@@ -7,6 +9,9 @@ public class Resume implements Comparable<Resume> {
     private final String uuid;
 
     private final String fullName;
+
+    private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
+    private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
@@ -23,6 +28,14 @@ public class Resume implements Comparable<Resume> {
 
     public String getFullName() {
         return fullName;
+    }
+
+    public void addContact(ContactType type, String value) {
+        contacts.put(type, value);
+    }
+
+    public void addSection(SectionType type, Section section) {
+        sections.put(type, section);
     }
 
     @Override
--- test/ru/mshatunov/basejava/storage/AbstractStorageTest.java
+++ test/ru/mshatunov/basejava/storage/AbstractStorageTest.java
@@ -5,26 +5,54 @@ import org.junit.Test;
 import ru.mshatunov.basejava.exception.ResumeAlreadyExistsStorageException;
 import ru.mshatunov.basejava.exception.ResumeNotExistsStorageException;
 import ru.mshatunov.basejava.exception.StorageException;
-import ru.mshatunov.basejava.model.Resume;
+import ru.mshatunov.basejava.model.*;
+
+import java.time.Month;
 
 import static org.junit.Assert.*;
 
 public abstract class AbstractStorageTest {
 
     static final String UUID_0 = "uuid0";
-    static final Resume RESUME_0 = new Resume(UUID_0, "Name");
+    static final Resume RESUME_0 = new Resume(UUID_0, "Name0");
 
     static final String UUID_1 = "uuid1";
-    static final Resume RESUME_1 = new Resume(UUID_1, "Name");
+    static final Resume RESUME_1 = new Resume(UUID_1, "Name1");
 
     static final String UUID_2 = "uuid2";
-    static final Resume RESUME_2 = new Resume(UUID_2, "Name");
+    static final Resume RESUME_2 = new Resume(UUID_2, "Name2");
 
     static final String UUID_3 = "uuid3";
-    static final Resume RESUME_3 = new Resume(UUID_3, "Name");
+    static final Resume RESUME_3 = new Resume(UUID_3, "Name3");
 
     static final String UUID_4 = "uuid4";
-    static final Resume RESUME_4 = new Resume(UUID_4, "Name");
+    static final Resume RESUME_4 = new Resume(UUID_4, "Name4");
+
+    static {
+        RESUME_1.addContact(ContactType.MAIL, "mail1@ya.ru");
+        RESUME_1.addContact(ContactType.PHONE, "11111");
+        RESUME_1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
+        RESUME_1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
+        RESUME_1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achivment11", "Achivment12", "Achivment13"));
+        RESUME_1.addSection(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));
+        RESUME_1.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization11", "http://Organization11.ru",
+                                new Organization.Position(2005, Month.JANUARY, "position1", "content1"),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "position2", "content2"))));
+        RESUME_1.addSection(SectionType.EDUCATION,
+                new OrganizationSection(
+                        new Organization("Institute", null,
+                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT facultet")),
+                        new Organization("Organization12", "http://Organization12.ru")));
+        RESUME_2.addContact(ContactType.SKYPE, "skype2");
+        RESUME_2.addContact(ContactType.PHONE, "22222");
+        RESUME_2.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization2", "http://Organization2.ru",
+                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
+    }
 
     Storage storage;
 
