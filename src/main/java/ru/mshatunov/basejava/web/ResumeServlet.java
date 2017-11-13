a882301dd1fbbc5c16520e5e09fcc10873bcf474
Maxim Shatunov <mshatunov@alfabank.ru>
2017-11-13 15:50:59 +0300

    Lesson 16. jsp

--- src/main/java/ru/mshatunov/basejava/web/ResumeServlet.java
+++ src/main/java/ru/mshatunov/basejava/web/ResumeServlet.java
@@ -1,8 +1,6 @@
 package ru.mshatunov.basejava.web;
 
 import ru.mshatunov.basejava.Config;
-import ru.mshatunov.basejava.model.ContactType;
-import ru.mshatunov.basejava.model.Resume;
 import ru.mshatunov.basejava.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -11,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -25,35 +22,8 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html");
-        Writer writer = resp.getWriter();
-        writer.write(
-                "<html>\n" +
-                        "<head>\n" +
-                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
-                        "    <title>Список всех резюме</title>\n" +
-                        "</head>\n" +
-                        "<body>\n" +
-                        "<section>\n" +
-                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
-                        "    <tr>\n" +
-                        "        <th>Имя</th>\n" +
-                        "        <th>Email</th>\n" +
-                        "    </tr>\n");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
-                            "     <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
-                            "</tr>\n");
-        }
-        writer.write("</table>\n" +
-                "</section>\n" +
-                "</body>\n" +
-                "</html>\n");
+        req.setAttribute("resumes", storage.getAllSorted());
+        req.getRequestDispatcher("WEB-INF/jsp/list.jsp").forward(req, resp);
     }
 
     @Override
